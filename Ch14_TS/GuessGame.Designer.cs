b4d980e65817c41cb23b52937d0c16805e85ad8c
Thad <Thadip@icloud.com>
2018-10-16 22:24:11 -0500

    Guessing Game Round One

---
+++ Ch14_TS/GuessGame.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Ch14_TS
+{
+    partial class GuessGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.highOrLowLabel = new System.Windows.Forms.Label();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.newGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mainLabel.Location = new System.Drawing.Point(12, 9);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(593, 50);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "I have a number between 1 and 1000--Can you guess my number?\r\nPlease enter your f" +
+    "irst guess.";
+            // 
+            // userInput
+            // 
+            this.userInput.Location = new System.Drawing.Point(15, 89);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(129, 22);
+            this.userInput.TabIndex = 1;
+            this.userInput.Text = "Enter # here";
+            // 
+            // highOrLowLabel
+            // 
+            this.highOrLowLabel.AutoSize = true;
+            this.highOrLowLabel.Location = new System.Drawing.Point(199, 89);
+            this.highOrLowLabel.Name = "highOrLowLabel";
+            this.highOrLowLabel.Size = new System.Drawing.Size(0, 17);
+            this.highOrLowLabel.TabIndex = 2;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(15, 140);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(88, 23);
+            this.submitButton.TabIndex = 3;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(288, 140);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // newGame
+            // 
+            this.newGame.Location = new System.Drawing.Point(497, 140);
+            this.newGame.Name = "newGame";
+            this.newGame.Size = new System.Drawing.Size(108, 23);
+            this.newGame.TabIndex = 5;
+            this.newGame.Text = "New Game";
+            this.newGame.UseVisualStyleBackColor = true;
+            this.newGame.Click += new System.EventHandler(this.newGame_Click);
+            // 
+            // GuessGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(671, 173);
+            this.Controls.Add(this.newGame);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.highOrLowLabel);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.mainLabel);
+            this.Name = "GuessGame";
+            this.Text = "Guess The Number";
+            this.Load += new System.EventHandler(this.GuessGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Label highOrLowLabel;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button newGame;
+    }
+}
+
---
+++ Ch14_TS/GuessGame.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch14_TS
+{
+    public partial class GuessGame : Form
+    {
+        // difference and random
+        static int diff = 0;
+        static int randomnumber;
+
+        public GuessGame()
+        {
+            InitializeComponent();
+        }
+        // Upon form load call startNew() 
+        private void GuessGame_Load(object sender, EventArgs e)
+        {
+            startNew();
+        }
+        // startNew() sets properties and creates new random
+        private void startNew()
+        {
+            Random random = new Random();
+            randomnumber = random.Next(1, 1001);
+            userInput.Enabled = true;
+            highOrLowLabel.Text = "";
+            userInput.BackColor = Color.Wheat;
+            userInput.Text = "Enter a number";
+            userInput.Focus();
+        }
+        // Upon click
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.White;
+            // Catch format errors
+            try
+            {
+                int userGuess = int.Parse(userInput.Text);
+                // Configure warmer and colder functionality
+                if (Math.Abs(randomnumber - userGuess) < diff)
+                {
+                    userInput.BackColor = Color.Red;
+                }
+                else
+                {
+                    userInput.BackColor = Color.LightBlue;
+                }
+                diff = Math.Abs(randomnumber - userGuess);
+                // Configure label functionality
+                if (userGuess < randomnumber)
+                {
+                    highOrLowLabel.Text = "Too Low";
+                }
+                if (userGuess > randomnumber)
+                {
+                    highOrLowLabel.Text = "Too High";
+                }
+                if (userGuess == randomnumber)
+                {
+                    userInput.BackColor = Color.Green;
+                    BackColor = Color.Green;
+                    MessageBox.Show("Correct");
+                    highOrLowLabel.Text = "Click New button to play again";
+                    userInput.Enabled = false;
+                    clearButton.Enabled = false;
+                    submitButton.Enabled = false;
+                }
+                // Prohibit out of bounds 
+                if (userGuess < 0 || userGuess > 1000)
+                {
+                    highOrLowLabel.Text = "Incorrect input";
+                    BackColor = Color.Crimson;
+                    userInput.BackColor = Color.Black;
+                }
+            }
+            // Catch format exception
+            catch (FormatException)
+            {
+                highOrLowLabel.Text = "Must be an integer between 0 and 1000";
+                BackColor = Color.Red;
+                userInput.BackColor = Color.Black;
+            }
+        }
+            private void clearButton_Click(object sender, EventArgs e)
+        {
+            userInput.Text = "";
+            userInput.Focus();
+        }
+        // newGame button click calls startNew()
+        private void newGame_Click(object sender, EventArgs e)
+        {
+            startNew();
+        }
+
+    }
+}
